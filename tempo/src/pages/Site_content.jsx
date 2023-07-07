75b7536077c291c0c8452aeb1c1968493755e937
Nitheshwaran <nitheshwaran003@gmail.com>
2023-07-07 13:04:42 +0530

    status count

--- tempo/src/pages/Site_content.jsx
+++ tempo/src/pages/Site_content.jsx
@@ -154,12 +154,10 @@ const Site_content = () => {
     useEffect(() => {
         if (activeCount < 10) {
             setisless_than_10_active(true)
-            // console.log("t");
 
         }
         else {
             setisless_than_10_active(false);
-            // console.log("hai");
         }
         if (inactiveCount < 10) {
             setisgreater_than_10_inactive(true)
@@ -167,7 +165,7 @@ const Site_content = () => {
         else {
             setisgreater_than_10_inactive(false);
         }
-    }, [])
+    })
 
 
     const site_edit_page = async (data) => {
